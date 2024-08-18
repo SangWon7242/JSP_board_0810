1f8933b224aee93993f89df7a4a5da910d768f30
SangWon7242 <gotkdqja@gmail.com>
2024-08-18 12:26:34 +0900

    게시물 삭제기능 구현

--- src/main/java/sbs/com/jsp/board/article/ArticleController.java
+++ src/main/java/sbs/com/jsp/board/article/ArticleController.java
@@ -68,4 +68,24 @@ public class ArticleController {
 
     rq.view("usr/article/detail");
   }
+
+  public void doDelete(Rq rq) {
+    long id = rq.getLongPathValueByIndex(1, 0);
+
+    if(id == 0) {
+      rq.appendBody("번호를 입력해주세요.");
+      return;
+    }
+
+    Article article = articleService.findById(id);
+
+    if(article == null) {
+      rq.appendBody("%d번 게시물은 존재하지 않습니다.".formatted(id));
+      return;
+    }
+
+    articleService.delete(id);
+
+    rq.appendBody("%d번 게시물이 삭제되었습니다.".formatted(id));
+  }
 }
--- src/main/java/sbs/com/jsp/board/article/ArticleRepository.java
+++ src/main/java/sbs/com/jsp/board/article/ArticleRepository.java
@@ -47,4 +47,12 @@ public class ArticleRepository {
 
     return null;
   }
+
+  public void delete(long id) {
+    Article article = findById(id);
+
+    if(article == null) return;
+
+    datum.remove(article);
+  }
 }
